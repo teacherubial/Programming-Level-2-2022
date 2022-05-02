44e3d20438cd5179cb17102e3d47f9268d5c4e72
Tim Ubial <tubial@sd38.bc.ca>
2022-05-02 09:21:06 -0700

    Added introtocollision.py and created a new Player class

---
+++ introtocollision.py
@@ -0,0 +1,65 @@
+# introtocollision.py
+
+import pygame
+
+# ----- CONSTANTS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+SKY_BLUE = (95, 165, 228)
+WIDTH = 800
+HEIGHT = 600
+TITLE = "Intro to Collision"
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Image
+        self.image = pygame.image.load("./assets/mario.png")
+        self.image = pygame.transform.scale(self.image, (19, 19))   # scale
+
+        # Rect
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Follow the mouse"""
+        self.rect.center = pygame.mouse.get_pos()
+
+
+def main():
+    pygame.init()
+
+    # ----- SCREEN PROPERTIES
+    size = (WIDTH, HEIGHT)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption(TITLE)
+
+    # ----- LOCAL VARIABLES
+    done = False
+    clock = pygame.time.Clock()
+
+
+
+    # ----- MAIN LOOP
+    while not done:
+        # -- Event Handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----- LOGIC
+
+        # ----- RENDER
+        screen.fill(BLACK)
+
+        # ----- UPDATE DISPLAY
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
